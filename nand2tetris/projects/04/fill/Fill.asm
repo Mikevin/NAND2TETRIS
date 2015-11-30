0dcb5ea64d0c48dcecfc0aa0ddbbda4009b22bcb
Mike van Leeuwen <mkvanleeuwen@gmail.com>
2015-12-01 00:49:50 +0100

    Fill almost working. Not sure if unfinished or screen is buggy.

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -3,9 +3,49 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
+// screen is 16384 - 24576 (8K)
+(LOOP)
+      @R0
+      M=0
+      @SCREEN
+      D=A
+      @R1
+      M=D
 
-// Put your code here.
+      @KBD
+      D=M
+      @SETBLACK
+      D;JNE
+      @FILL
+      0;JMP
+      @LOOP
+      D;JEQ
+(SETBLACK)
+      @32767
+      D=A
+      @R0
+      M=D
+      @FILL
+      0;JMP
+(FILL)
+      @R1
+      D=M
+      @24576
+      D=D-A
+      @LOOP
+      D;JEQ //check if whole screen filled
+
+      @R0
+      D=M
+      @R1
+      A=M
+      M=D
+      //up the pixel counter
+      @R1
+      M=M+1
+      @FILL
+      0;JMP
